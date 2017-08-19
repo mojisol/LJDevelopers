6339fa909247fb6065e14b4f4ae76ff0cbc3e7f1
Mojisola Omolumo <mojisolaomolumo@gmail.com>
2017-08-19 18:02:06 +0100

    Create Layout File

---
+++ app/src/main/java/com/ljd/android/ljdevelopers/MainActivity.java
@@ -0,0 +1,20 @@
+package com.ljd.android.ljdevelopers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager (this));
+        recyclerView.setHasFixedSize(true);
+    }
+}
